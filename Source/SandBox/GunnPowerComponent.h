9253b144970c59a52ba1b8fb295334eeaa839f1c
bouillon2poulet <romain.serres74@gmail.com>
2024-10-04 23:59:43 +0200

    GunnPowers UI prototype
    Renamed assets

---
+++ Source/SandBox/GunnPowerComponent.h
@@ -0,0 +1,27 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "GunnPowerComponent.generated.h"
+
+
+UCLASS(Blueprintable, ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class SANDBOX_API UGunnPowerComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this component's properties
+	UGunnPowerComponent();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType,
+	                           FActorComponentTickFunction* ThisTickFunction) override;
+};
---
+++ Source/SandBox/Public/ClassToTextureDataAsset.h
@@ -0,0 +1,45 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "ClassToTextureDataAsset.generated.h"
+
+USTRUCT(BlueprintType)
+struct FClassToTexture
+{
+	GENERATED_BODY()
+	UPROPERTY(EditDefaultsOnly)
+	TSubclassOf<UActorComponent> Class;
+	UPROPERTY(EditDefaultsOnly)
+	UTexture2D* Texture;
+};
+
+/**
+ * 
+ */
+UCLASS(BlueprintType)
+class SANDBOX_API UClassesToTexturesDataAsset : public UPrimaryDataAsset
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditDefaultsOnly)
+	TArray<FClassToTexture> ClassesToTextures;
+
+	UFUNCTION(BlueprintCallable)
+	UTexture2D* GetTextureFromClass(TSubclassOf<UActorComponent> Class);
+};
+
+inline UTexture2D* UClassesToTexturesDataAsset::GetTextureFromClass(TSubclassOf<UActorComponent> Class)
+{
+	for (FClassToTexture ClassToTexture : ClassesToTextures)
+	{
+		if (ClassToTexture.Class == Class)
+		{
+			return ClassToTexture.Texture;
+		}
+	}
+	return nullptr;
+}
