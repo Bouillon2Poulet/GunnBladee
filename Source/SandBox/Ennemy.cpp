b282e487e2475ef4879dadcfb4f39ee374d6ea0f
bouillon2poulet <romain.serres74@gmail.com>
2024-10-09 00:15:06 +0200

    First ennemy and health bar

---
+++ Source/SandBox/Ennemy.cpp
@@ -0,0 +1,7 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Ennemy.h"
+
+
+// Add default functionality here for any IEnnemy functions that are not pure virtual.
---
+++ Source/SandBox/Ennemy.h
@@ -0,0 +1,27 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "Ennemy.generated.h"
+
+// This class does not need to be modified.
+UINTERFACE(Blueprintable)
+class UEnnemy : public UInterface
+{
+	GENERATED_BODY()
+};
+
+/**
+ * 
+ */
+class SANDBOX_API IEnnemy
+{
+	GENERATED_BODY()
+
+	// Add interface functions to this class. This is the class that will be inherited to implement this interface.
+public:
+	UFUNCTION(BlueprintImplementableEvent, BlueprintCallable)
+	int GetOverlapDamages();
+};
